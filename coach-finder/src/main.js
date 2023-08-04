7a20b8f01dc9c2d1d4545fb860140289939be7d1
Cepa95 <ceprnic.josip@gmail.com>
2023-08-04 10:28:48 +0200

    adding vuex

--- coach-finder/src/main.js
+++ coach-finder/src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-import router from "./router.js"
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue';
 
 const app = createApp(App)
 
 app.use(router);
-app.mount('#app')
+app.use(store);
+
+app.mount('#app');
