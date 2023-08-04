f6f590818a9bf6d920baff86fa16e7d0f7d42fe2
Cepa95 <ceprnic.josip@gmail.com>
2023-08-05 00:08:30 +0200

    ui/base

--- coach-finder/src/components/coaches/CoachItem.vue
+++ coach-finder/src/components/coaches/CoachItem.vue
@@ -1,38 +1,33 @@
 <template>
   <li>
-    <h3>
-      {{ fullName }}
-    </h3>
-    <h4>${{ rate }} hour</h4>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
     <div>
-      <span v-for="area in areas" :key="area"> {{ area }}</span>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
     </div>
     <div class="actions">
-      <router-link :to="coachContactLink"> Contact</router-link>
-      <router-link :to="coachDetailsLink"> View Details</router-link>
+      <base-button mode="outline" link :to="coachContactLink">Contact</base-button>
+      <base-button link :to="coachDetailsLink">View Details</base-button>
     </div>
   </li>
 </template>
 
-
 <script>
 export default {
-  props: ["id", "firstName", "lastName", "rate", "areas"],
+  props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
   computed: {
     fullName() {
-      return this.firstName + " " + this.lastName;
+      return this.firstName + ' ' + this.lastName;
     },
-    coachContactLink(){
-        return  this.$route.path + '/' +this.id + '/contact'
+    coachContactLink() {
+      return this.$route.path + '/' + this.id + '/contact'; // /coaches/c1/contact
     },
-
-    coachDetailsLink(){
-        return this.$route.path + '/' + this.id;
-    }
+    coachDetailsLink() {
+      return this.$route.path + '/' + this.id; // /coaches/c1
+    },
   },
 };
 </script>
-
 
 <style scoped>
 li {
--- coach-finder/src/pages/coaches/CoachesList.vue
+++ coach-finder/src/pages/coaches/CoachesList.vue
@@ -1,9 +1,10 @@
 <template>
   <section>FILTER</section>
   <section>
+    <base-card>
     <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
+      <base-button mode="outline">Refresh</base-button>
+      <base-button link to="/register">Register as Coach</base-button>
     </div>
     <ul v-if="hasCoaches">
       <coach-item
@@ -13,18 +14,22 @@
         :first-name="coach.firstName"
         :last-name="coach.lastName"
         :rate="coach.hourlyRate"
+        :areas="coach.areas"
       ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
+  </base-card>
   </section>
 </template>
  
  <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
+import BaseButton from '../../components/ui/BaseButton.vue';
 
 export default {
   components: {
     CoachItem,
+    BaseButton,
   },
   computed: {
     filteredCoaches() {
