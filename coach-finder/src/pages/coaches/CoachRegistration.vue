8c424683cdf0242d7f49be72d486b942606ed191
Cepa95 <ceprnic.josip@gmail.com>
2023-08-07 00:03:45 +0200

    coaches

--- coach-finder/src/pages/coaches/CoachRegistration.vue
+++ coach-finder/src/pages/coaches/CoachRegistration.vue
@@ -1,17 +1,23 @@
 <template>
-    <section>
-        <base-card>
-        <h2> Register as a coach now!</h2>
-        <coach-form></coach-form>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
     </base-card>
-    </section>
+  </section>
 </template>
 
 <script>
-import CoachForm from "../../components/coaches/CoachForm.vue"
+import CoachForm from "../../components/coaches/CoachForm.vue";
 export default {
-    components: {
-        CoachForm
-    }
-}
+  components: {
+    CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch("coaches/registerCoach", data);
+      this.$router.replace("/coaches");
+    },
+  },
+};
 </script>
