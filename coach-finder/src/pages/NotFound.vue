2b1985fef970484aaa3faae6aa4f4c4234487836
Cepa95 <ceprnic.josip@gmail.com>
2023-08-10 18:38:50 +0200

    not found

--- coach-finder/src/pages/NotFound.vue
+++ coach-finder/src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>not found</h1>
+  <section>
+    <base-card>
+      <h2>Page not found!</h2>
+      <p>Return to<router-link to="/coaches"> main page.</router-link></p>
+    </base-card>
+  </section>
 </template>
