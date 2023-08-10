51d4e767db2a5dad20acc419aa771bfc8bd2eb0e
Cepa95 <ceprnic.josip@gmail.com>
2023-08-11 00:18:58 +0200

    auth&validation

---
+++ coach-finder/src/pages/auth/UserAuth.vue
@@ -0,0 +1,164 @@
+<template>
+  <base-card>
+    <form @submit.prevent="submitForm">
+      <div class="form-control" :class="{ invalid: !emailIsValid }">
+        <label for="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          v-model.trim="email"
+          @blur="clearValidity('email')"
+        />
+      </div>
+      <p v-if="!emailIsValid">E-mail is not valid!</p>
+      <div class="form-control" :class="{ invalid: !passwordIsValid }">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          v-model.trim="password"
+          @blur="clearValidity('password')"
+        />
+      </div>
+      <p v-if="!passwordIsValid">Password must be at least 6 characters</p>
+      <p v-if="!formIsValid">Please fix the errors</p>
+      <base-button>{{ submitButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchAuthMode">
+        {{ switchModeButtonCaption }}</base-button
+      >
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      formIsValid: true,
+      emailIsValid: true,
+      passwordIsValid: true,
+      mode: "login",
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === "login") {
+        return "Login";
+      } else {
+        return "Signup";
+      }
+    },
+    switchModeButtonCaption() {
+      if (this.mode === "login") {
+        return "Signup instead";
+      } else {
+        return "Login instead";
+      }
+    },
+  },
+  methods: {
+    submitForm() {
+      console.log("provjera");
+      console.log(this.password);
+      console.log(this.email);
+
+      this.formIsValid = true;
+      this.emailIsValid = true;
+      this.passwordIsValid = true;
+
+      // Regular expression for basic email validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!emailRegex.test(this.email)) {
+        this.emailIsValid = false;
+        this.formIsValid = false;
+      }
+
+      if (this.password.length < 6) {
+        this.passwordIsValid = false;
+        this.formIsValid = false;
+      }
+    },
+    switchAuthMode() {
+      if (this.mode === "login") {
+        this.mode = "signup";
+      } else {
+        this.mode = "login";
+      }
+    },
+    clearValidity(input) {
+      if (input === "email") {
+        this.emailIsValid = true;
+      } else {
+        this.passwordIsValid = true;
+      }
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+p {
+  font-weight: bold;
+  color: #800000;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input {
+  border: 1px solid red;
+}
+</style>
--- coach-finder/src/router.js
+++ coach-finder/src/router.js
@@ -5,7 +5,8 @@ import CoachesList from "./pages/coaches/CoachesList.vue";
 import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
 import ContactCoach from "./pages/requests/ContactCoach.vue";
 import RequestsReceived from "./pages/requests/RequestsReceived.vue";
-import NotFound from "./pages/NotFound.vue"
+import NotFound from "./pages/NotFound.vue";
+import UserAuth from "./pages/auth/UserAuth.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,6 +21,7 @@ const router = createRouter({
     },
     { path: "/register", component: CoachRegistration },
     { path: "/requests", component: RequestsReceived },
+    { path: "/auth", component: UserAuth },
     { path: "/:notFound(.*)", component: NotFound },
   ],
 });
